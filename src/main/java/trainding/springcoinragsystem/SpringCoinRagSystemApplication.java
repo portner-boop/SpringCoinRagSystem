1756950c094d5adabb7599b8362fc3667de73c43
portner-boop <damirmagdeev1054@gmail.com>
2025-09-23 20:31:21 +0400

    Initial commit

---
+++ src/main/java/trainding/springcoinragsystem/SpringCoinRagSystemApplication.java
@@ -0,0 +1,13 @@
+package trainding.springcoinragsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoinRagSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoinRagSystemApplication.class, args);
+	}
+
+}
